d00a40af05f9d236cbbf2e6dac2942eaed3999f3
“Atinuke <atinukeogunbannjo@gmail.com>
2025-07-30 15:03:54 +0100

    integrated cypress

---
+++ cypress/e2e/welcome.cy.js
@@ -0,0 +1,9 @@
+describe('Verifying that our page is LIVE', () => {
+  it('it visits the live app', () => {
+    cy.visit('https://securityy.vercel.app/')
+
+    cy.contains('EDO STATE SECURITY SUMMIT').should('be.visible')
+    
+  })
+
+})
