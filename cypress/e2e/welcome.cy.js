b57d7db53b2cbd4c1fd7bce2632b3155d5c21027
“Atinuke <atinukeogunbannjo@gmail.com>
2025-08-13 10:27:31 +0100

    Added navigation test

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -9,29 +9,60 @@
 // })
 
 describe('NNavigation Test', () => {
-  it('it visits the live app', () => {
-    cy.visit('https://securityy.vercel.app/')
+  beforeEach(() => {
+
+  cy.visit('https://securityy.vercel.app/')}) 
+  it('Navigate to the event page', () => {
+   
     cy.contains('Event').click()
     cy.url().should('include', '/event')
     cy.contains('THE EDO STATE SECURITY SUMMIT').should('be.visible')
 
-    cy.contains('Speakers').click()
-    cy.url().should('include', '/speakers')
-    cy.contains('THE EDO STATE SECURITY SUMMIT').should('be.visible')
+    // cy.contains('Speakers').click()
+    // cy.url().should('include', '/speakers')
+    // cy.contains('THE EDO STATE SECURITY SUMMIT').should('be.visible')
 
-    cy.contains('Gallery').click()
-    cy.url().should('include', '/gallery')
-    cy.contains('Our Gallery').should('be.visible')
+    // cy.contains('Gallery').click()
+    // cy.url().should('include', '/gallery')
+    // cy.contains('Our Gallery').should('be.visible')
 
-    cy.contains('Services').click()
-    cy.url().should('include', '/services')
-    cy.contains('Ministry of Public Security and Safety').should('be.visible')
+    // cy.contains('Services').click()
+    // cy.url().should('include', '/services')
+    // cy.contains('Ministry of Public Security and Safety').should('be.visible')
 
-    cy.contains('Contact').click()
-    cy.url().should('include', '/contact')
+    // cy.contains('Contact').click()
+    // cy.url().should('include', '/contact')
 
-    cy.contains('FOR ENQUIRES!').should('be.visible')
+    // cy.contains('FOR ENQUIRES!').should('be.visible')
     
   })
 
+  it('Navigate to the speakers page', () => {
+    cy.contains('Speakers').click()
+    cy.url().should('include', '/speakers')
+    cy.contains('THE EDO STATE SECURITY SUMMIT').should('be.visible')
+  })
+
+  it('Navigate to the Gallery page', () => { 
+     cy.contains('Gallery').click()
+    cy.url().should('include', '/gallery')
+    cy.contains('Our Gallery').should('be.visible')
+
+  })
+
+   it('Navigate to the Services page', () => { 
+     cy.contains('Services').click()
+    cy.url().should('include', '/services')
+    cy.contains('Ministry of Public Security and Safety').should('be.visible')
+
+  })
+
+   it('Navigate to the Contact page', () => { 
+     cy.contains('Contact').click()
+    cy.url().should('include', '/contact')
+
+    cy.contains('FOR ENQUIRES!').should('be.visible')
+  })
+    
+  
 })
